acb881777081863e9ac8fc489b2b836fcc60fb0a
Tobias B <abcd@email.de>
2025-09-25 13:01:04 +0200

    Tag3ende

--- box.py
+++ box.py
@@ -1,4 +1,6 @@
+# start
 name = "Myname"
 country = "germany"
-age = "38"
-print("Hello my name is "name" and i am "age" old and i come from "country"")
+age = 38
+print(f"Hello my name is {name} and i am {age} old and i come from {country}")
+# end
